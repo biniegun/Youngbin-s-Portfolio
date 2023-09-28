af8edc4037abaf9a5ee9ad95f63fff2fa6ee2a65
biniegun <143500896+biniegun@users.noreply.github.com>
2023-09-28 13:31:58 +0400

    Observe - Part2

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
   // 스크롤되는 y 좌표가 headerHeight보다 크다면 다른 스타일링!
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
 
   if(window.scrollY > headerHeight) {
     header.classList.add('header--dark');
